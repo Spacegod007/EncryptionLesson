05e8c9649ea19ca047efab52a6e8ad8348aab840
GeniseTop2 <springtouw@hotmail.com>
2018-06-19 14:19:08 +0200

    style fix

--- salt/src/logic/PasswordBasedEncryption.java
+++ salt/src/logic/PasswordBasedEncryption.java
@@ -12,8 +12,7 @@ import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Arrays;
 
-public class PasswordBasedEncryption
-{
+public class PasswordBasedEncryption {
     private static final String KEY_DERIVATION_FUNCTION = "PBKDF2WithHmacSHA256";
     private static final String ENCRYPTION_ALGORITHM = "AES";
     private static final String TRANSFORMATION = "AES/GCM/NoPadding";
@@ -24,14 +23,11 @@ public class PasswordBasedEncryption
     private static final int ITERATION_COUNT = 200_000;
     private static final int SALT_SIZE_IN_BITS = 1024;
 
-    private PasswordBasedEncryption()
-    {
+    private PasswordBasedEncryption() {
     }
 
-    static byte[] encrypt(byte[] message, char[] password)
-    {
-        try
-        {
+    static byte[] encrypt(byte[] message, char[] password) {
+        try {
             byte[] salt = generateIV();
             Cipher cipher = initCipher(Cipher.ENCRYPT_MODE, password, salt);
             byte[] ciphertext = cipher.doFinal(message);
@@ -40,68 +36,49 @@ public class PasswordBasedEncryption
             System.arraycopy(ciphertext, 0, result, salt.length, ciphertext.length);
 
             return result;
-        }
-        catch (IllegalBlockSizeException | BadPaddingException ex)
-        {
+        } catch (IllegalBlockSizeException | BadPaddingException ex) {
             throw new AssertionError(ex);
         }
     }
 
-    private static Cipher initCipher(int mode, char[] password, byte[] iv)
-    {
+    private static Cipher initCipher(int mode, char[] password, byte[] iv) {
         GCMParameterSpec gcmSpec = new GCMParameterSpec(TAG_SIZE_IN_BITS, iv);
         SecretKey key = deriveKey(password, iv);
-        try
-        {
+        try {
 
             Cipher cipher = Cipher.getInstance(TRANSFORMATION);
             cipher.init(mode, key, gcmSpec);
             return cipher;
         } catch (
-                NoSuchAlgorithmException |
-                        NoSuchPaddingException |
-                        InvalidKeyException |
-                        InvalidAlgorithmParameterException ex
-                )
-        {
+                NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException ex) {
             throw new AssertionError(ex);
-        } finally
-        {
-            try
-            {
+        } finally {
+            try {
                 key.destroy();
             }
 
             // Don't let exceptions escape from finally-blocks.
-            catch (DestroyFailedException ex)
-            {
+            catch (DestroyFailedException ex) {
             }
         }
     }
 
-    private static SecretKey deriveKey(char[] password, byte[] salt)
-    {
+    private static SecretKey deriveKey(char[] password, byte[] salt) {
         PBEKeySpec pbeKeySpec = new PBEKeySpec(password, salt, ITERATION_COUNT, KEY_SIZE_IN_BITS);
         SecretKey pbeKey = null;
         byte[] keyBytes = null;
 
-        try
-        {
+        try {
             SecretKeyFactory factory = SecretKeyFactory.getInstance(KEY_DERIVATION_FUNCTION);
             pbeKey = factory.generateSecret(pbeKeySpec);
 
             keyBytes = pbeKey.getEncoded();
             return new SecretKeySpec(keyBytes, ENCRYPTION_ALGORITHM);
         } catch (
-                NoSuchAlgorithmException |
-                        InvalidKeySpecException ex
-                )
-        {
+                NoSuchAlgorithmException | InvalidKeySpecException ex) {
             throw new AssertionError(ex);
-        } finally
-        {
-            try
-            {
+        } finally {
+            try {
                 pbeKeySpec.clearPassword();
 
                 if (keyBytes != null)
@@ -109,16 +86,13 @@ public class PasswordBasedEncryption
 
                 if (pbeKey != null && !pbeKey.isDestroyed())
                     pbeKey.destroy();
-            } catch (DestroyFailedException ex)
-            {
+            } catch (DestroyFailedException ex) {
             }
         }
     }
 
-    public static byte[] decryprion(byte[] encrypted, char[] password)
-    {
-        try
-        {
+    public static byte[] decryprion(byte[] encrypted, char[] password) {
+        try {
             byte[] salt = Arrays.copyOfRange(encrypted, 0, SALT_SIZE_IN_BITS / 8);
             byte[] ciphertext = Arrays.copyOfRange(encrypted, salt.length, encrypted.length);
 
@@ -129,26 +103,21 @@ public class PasswordBasedEncryption
         }
 
         // Block sizes are a property of block ciphers, not stream ciphers.
-        catch (IllegalBlockSizeException ex)
-        {
+        catch (IllegalBlockSizeException ex) {
             throw new AssertionError(ex);
-        } catch (BadPaddingException e)
-        {
+        } catch (BadPaddingException e) {
             e.printStackTrace();
         }
         return new byte[0];
     }
 
-    private static byte[] generateIV()
-    {
-        try
-        {
+    private static byte[] generateIV() {
+        try {
             SecureRandom random = SecureRandom.getInstance(GENERATOR_ALGORITHM);
             byte[] salt = new byte[SALT_SIZE_IN_BITS / 8];
             random.nextBytes(salt);
             return salt;
-        } catch (NoSuchAlgorithmException e)
-        {
+        } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
         return new byte[0];
